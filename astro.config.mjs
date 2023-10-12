c6303f3f88891f35dcec026489ddffe98e22f65f
Hiroshi Urabe <mail@torounit.com>
2023-10-13 02:53:52 +0900

    add single page and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(
-    {
-      runtime: 'local',
-    }
-  )
+  adapter: cloudflare({
+    runtime: 'local'
+  }),
+  integrations: [tailwind()]
 });
